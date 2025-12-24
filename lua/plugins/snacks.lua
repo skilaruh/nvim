d6ba364a8eeb2ea57b307b681f5f24cd73f25551
skilaruh <85468370+skilaruh@users.noreply.github.com>
2025-12-24 18:09:01 -0500

    Disabled notifier

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
         indent = { enabled = true },
         input = { enabled = true },
         notifier = {
-            enabled = true,
+            enabled = false,
             timeout = 1000,
         },
         picker = { enabled = true },
