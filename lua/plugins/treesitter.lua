1a2edfdda0a8900cee79024db1e3a9c15499ec43
skilaruh <85468370+skilaruh@users.noreply.github.com>
2026-07-26 20:46:09 -0400

    Updated treesitter entries

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,61 @@
 local treesitter = require("nvim-treesitter")
 
-vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-        local lang = vim.treesitter.language.get_lang(args.match)
 
-        if not lang then
-            return
-        end
-
-        if not vim.tbl_contains(treesitter.get_available(), lang) then
-            return
-        end
-
-        if not vim.tbl_contains(treesitter.get_installed(), lang) then
-            treesitter.install({ lang })
-        end
-    end,
+treesitter.install({
+    "asm",
+    "bash",
+    "c",
+    "cmake",
+    "cpp",
+    "css",
+    "csv",
+    "cuda",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "go",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "latex",
+    "llvm",
+    "lua",
+    "luadoc",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "r",
+    "requirements",
+    "rust",
+    "sql",
+    "toml",
+    "typescript",
+    "vimdoc",
+    "yaml",
+    "zig",
 })
+
+
+
+-- Auto installs parsers on new filetypes
+-- vim.api.nvim_create_autocmd("FileType", {
+--     callback = function(args)
+--         local lang = vim.treesitter.language.get_lang(args.match)
+--
+--         if not lang then
+--             return
+--         end
+--
+--         if not vim.tbl_contains(treesitter.get_available(), lang) then
+--             return
+--         end
+--
+--         if not vim.tbl_contains(treesitter.get_installed(), lang) then
+--             treesitter.install({ lang })
+--         end
+--     end,
+-- })
